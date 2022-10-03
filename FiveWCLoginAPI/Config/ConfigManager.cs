1647a6df40fc4c309c1bf16c222d2df4418b105e
hburn7 <hburnett777@gmail.com>
2022-10-03 15:07:10 -0400

    db string change (wip)

--- FiveWCLoginAPI/Config/ConfigManager.cs
+++ FiveWCLoginAPI/Config/ConfigManager.cs
@@ -4,11 +4,14 @@ public class ConfigManager : IConfigManager
 {
 	private readonly IConfiguration _configuration;
 	public ConfigManager(IConfiguration configuration) { _configuration = configuration; }
-	public string DbHost => IsDocker ? "host.docker.internal" : _configuration["Database:Host"];
+	public bool IsDocker => bool.Parse(_configuration["Flags:IsDocker"]);
+	public string DbHost => _configuration["Database:Host"];
 	public int DbPort => int.Parse(_configuration["Database:Port"]);
 	public string DbUsername => _configuration["Database:Username"];
 	public string DbPassword => _configuration["Database:Password"];
 	public string DbName => _configuration["Database:Database"];
-	public bool IsDocker => bool.Parse(_configuration["Flags:IsDocker"]);
-	public string GetConnectionString() => $"Host={DbHost}:{DbPort};Username={DbUsername};Password={DbPassword};Database={DbName}";
+
+	public string GetConnectionString() => IsDocker
+		? $"postgresql://{DbUsername}:{DbPassword}@db/{DbName}"
+		: $"postgresql://{DbUsername}:{DbPassword}@{DbHost}:{DbPort}/{DbName}";
 }
