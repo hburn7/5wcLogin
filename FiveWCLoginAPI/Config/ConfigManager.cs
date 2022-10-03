156d4a44d6c7079ef347ab7ef051d83181a1a9a5
hburn7 <hburnett777@gmail.com>
2022-10-03 14:40:43 -0400

    IsDocker database flag

--- FiveWCLoginAPI/Config/ConfigManager.cs
+++ FiveWCLoginAPI/Config/ConfigManager.cs
@@ -4,10 +4,11 @@ public class ConfigManager : IConfigManager
 {
 	private readonly IConfiguration _configuration;
 	public ConfigManager(IConfiguration configuration) { _configuration = configuration; }
-	public string DbHost => _configuration["Database:Host"];
+	public string DbHost => IsDocker ? "host.docker.internal" : _configuration["Database:Host"];
 	public int DbPort => int.Parse(_configuration["Database:Port"]);
 	public string DbUsername => _configuration["Database:Username"];
 	public string DbPassword => _configuration["Database:Password"];
 	public string DbName => _configuration["Database:Database"];
+	public bool IsDocker => bool.Parse(_configuration["Flags:IsDocker"]);
 	public string GetConnectionString() => $"Host={DbHost}:{DbPort};Username={DbUsername};Password={DbPassword};Database={DbName}";
 }
