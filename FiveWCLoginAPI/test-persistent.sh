498cbe94028713ee8c13a8233f3a7e7aa8c2a520
hburn7 <hburnett777@gmail.com>
2022-12-14 21:42:39 -0500

    db test script

---
+++ FiveWCLoginAPI/test-persistent.sh
@@ -0,0 +1,5 @@
+﻿#!/bin/bash
+# Test persistent database using docker
+# shell script to run docker with volume using postgres.
+# shellcheck disable=SC2215
+docker run -d -p 5432:5432 --name 5wc-test-persistent -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=five_wc_api -v persistent-test:/var/lib/postgresql/data postgres
